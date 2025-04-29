affc5fd87dbe31bfbe4c4d4acc2114d2eec54b67
chedighribi <chady.ghribi.gc@gmail.com>
2025-04-29 15:51:18 +0200

    feat: adding data to the cfp component

--- src/app/features/carbon-footprint/carbon-footprint.component.ts
+++ src/app/features/carbon-footprint/carbon-footprint.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import {CarbonFootprintFormComponent} from "./carbon-footprint-form/carbon-footprint-form.component";
 import {CarbonFootprintResultComponent} from "./carbon-footprint-result/carbon-footprint-result.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-carbon-footprint',
   standalone: true,
   imports: [
     CarbonFootprintFormComponent,
-    CarbonFootprintResultComponent
+    CarbonFootprintResultComponent,
+    CommonModule,
   ],
   templateUrl: './carbon-footprint.component.html',
   styleUrl: './carbon-footprint.component.css'
 })
 export class CarbonFootprintComponent {
-
+  distanceKm: number = 140;
+  consommationPour100Km: number = 10;
+  get consommationTotalKm(): number {
+    return (this.consommationPour100Km / 100) * this.distanceKm;
+  }
+  addDistance (){
+    return this.distanceKm +=100;
+  }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import {TitleCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    TitleCasePipe
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+   prenom: string = "chady";
 }
